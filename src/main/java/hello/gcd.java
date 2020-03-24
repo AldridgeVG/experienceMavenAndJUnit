436572a15b3f1fe08ee8e6afb6c4b67be5f7c439
aldridgevg <caxuser@live.com>
2020-03-24 20:39:14 +0800

    hello Maven

---
+++ src/main/java/hello/gcd.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+import java.util.Scanner;
+
+public class gcd {
+    public static void main(String[]args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("input 2 integers below:");
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        int a = ArithmeticUtils.gcd(m,n);
+        System.out.println("this is their greatest common division: "+a);
+    }
+}
